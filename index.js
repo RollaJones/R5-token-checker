9f68eea9bd734eff2c5d798a14c8433615b46aae
RollaJones <rollajoness@gmail.com>
2025-03-25 18:12:35 -0700

    Update index.js port fix

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+console.log("Detected PORT from environment:", PORT);
+
 app.use(cors());
 app.use(express.json());
-
 app.use('/api/scan', scanRoutes);
 
 app.listen(PORT, () => {
     console.log(`R5 Secure Token Checker API running on port ${PORT}`);
 });
-console.log("Detected PORT from environment:", process.env.PORT);
