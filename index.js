d74973b069249d27d0167fdb26451cdead6b5937
RollaJones <rollajoness@gmail.com>
2025-03-25 19:04:47 -0700

    Move backend files from Backend branch to main with proper structure

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const scanRoutes = require('./routes/scan');
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
-
-console.log("Detected PORT from environment:", PORT);
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
